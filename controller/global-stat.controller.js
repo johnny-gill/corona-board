2f971e28ebb0b88374fb6a4fbb501e27d20641ba
johnny gill <uint777@gmail.com>
2023-08-08 13:56:20 +0900

    컨트롤러 생성

---
+++ controller/global-stat.controller.js
@@ -0,0 +1,41 @@
+const { GlobalStat } = require('../database');
+
+const getAll = async (req, res) => {
+  const result = await GlobalStat.findAll();
+  res.status(200).json({ result });
+};
+
+const insertOrUpdate = async (req, res) => {
+  const { cc, date } = req.body;
+  if (!cc || !date) {
+    res.status(400).json({ error: 'cc and date are required' });
+    return;
+  }
+
+  const count = await GlobalStat.count({ where: { cc, date } });
+  if (count === 0) {
+    await GlobalStat.create(req.body);
+  } else {
+    await GlobalStat.update(req.body, { where: { cc, date } });
+  }
+
+  res.status(200).json({ result: 'success' });
+};
+
+const remove = async (req, res) => {
+  const { cc, date } = req.body;
+  if (!cc || !date) {
+    res.status(400).json({ error: 'cc and date are required' });
+    return;
+  }
+
+  await GlobalStat.destroy({ where: { cc, date } });
+
+  res.status(200).json({ result: 'success' });
+};
+
+module.exports = {
+  getAll,
+  insertOrUpdate,
+  remove,
+};
