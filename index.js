dca02a11d95ebff7e1fb2a9c357ced52b30b5c4b
johnny gill <uint777@gmail.com>
2023-08-08 12:01:14 +0900

    객체모듈 동기화

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { sequelize } = require('./database');
 
-const app = express();
-app.use(bodyParser.json());
+const launchServer = async () => {
+  const app = express();
+  app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello' });
-});
+  app.get('/', (req, res) => {
+    res.json({ message: 'Hello!!!' });
+  });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  try {
+    await sequelize.sync();
+    console.log('Database is ready!');
+  } catch (error) {
+    console.log('Unable to connect to the database');
+    console.log(error);
+    process.exit(1);
+  }
+
+  const port = process.env.PORT || 8080;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+launchServer();
