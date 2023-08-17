19ed80e793fe65737ef55b61184bc59870227db1
johnny gill <uint777@gmail.com>
2023-08-17 17:57:05 +0900

    api에서 데이터 불러오기 및 정적 웹사이트 빌드!!!

--- coronaboard-web/src/data-loader.js
+++ coronaboard-web/src/data-loader.js
@@ -1,10 +1,65 @@
 const _ = require('lodash');
-const countryInfo = require('../tools/downloaded/countryInfo.json');
+const axios = require('axios');
+const countryInfo = require('../../tools/downloaded/countryInfo.json');
+const { format, subDays } = require('date-fns');
+const { utcToZonedTime } = require('date-fns-tz');
 
 const getDataSource = async () => {
   const countryByCc = _.keyBy(countryInfo, 'cc');
+  const globalStats = await generateGlobalStats();
 
-  return { countryByCc };
+  return { globalStats, countryByCc };
+};
+
+const generateGlobalStats = async () => {
+  const apiClient = axios.create({
+    baseURL: process.env.CORONABOARD_API_BASE_URL || 'http://localhost:8080',
+  });
+
+  const response = await apiClient.get('global-stats');
+
+  const groupedByDate = _.groupBy(response.data.result, 'date');
+
+  const now = new Date('2021-06-05');
+  const timeZone = 'Asia/Seoul';
+  const today = format(utcToZonedTime(now, timeZone), 'yyyy-MM-dd');
+  const yesterday = format(
+    utcToZonedTime(subDays(now, 1), timeZone),
+    'yyyy-MM-dd'
+  );
+
+  if (!groupedByDate[today]) {
+    throw new Error('Data for today is missing');
+  }
+
+  return createGlobalStatWithPrevField(
+    groupedByDate[today],
+    groupedByDate[yesterday]
+  );
+};
+
+const createGlobalStatWithPrevField = (todayStats, yesterdayStats) => {
+  const yesterdayStatsByCc = _.keyBy(yesterdayStats, 'cc');
+
+  const globalStatWithPrev = todayStats.map((todayStat) => {
+    const cc = todayStat.cc;
+    const yesterdayStat = yesterdayStatsByCc[cc];
+
+    if (yesterdayStat) {
+      return {
+        ...todayStat,
+        confirmedPrev: yesterdayStat.confirmed || 0,
+        deathPrev: yesterdayStat.death || 0,
+        negativePrev: yesterdayStat.negative || 0,
+        releasedPrev: yesterdayStat.released || 0,
+        testedPrev: yesterdayStat.tested || 0,
+      };
+    }
+
+    return todayStat;
+  });
+
+  return globalStatWithPrev;
 };
 
 module.exports = {
--- coronaboard-web/src/templates/single-page.js
+++ coronaboard-web/src/templates/single-page.js
@@ -1,19 +1,17 @@
 import Slide from '../components/slide';
-import React from "react"
+import React from 'react';
 
 const SinglePage = ({ pageContext }) => {
   const { dataSource } = pageContext;
-  const { thirdSlideTitle } = dataSource;
+  const { countryByCc, globalStats } = dataSource;
+
+  console.log(countryByCc);
+  console.log(globalStats);
 
   return (
     <div>
       <h1>코로나보드</h1>
-      <p>createPage로 만들어진 페이지입니다.</p>
       <Slide title="국가별 현황">국가별 현황을 보여줍니다.</Slide>
-      <Slide title={'대한민국 지역별 현황'}>
-        대한민국 지역별 현황을 보여줍니다.
-      </Slide>
-      <Slide title={thirdSlideTitle}>예방 행동 수칙을 보여줍니다.</Slide>
     </div>
   );
 };
