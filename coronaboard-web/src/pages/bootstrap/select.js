91ca70c5c5a8166d5b6d3a0b522a5ae2099e9d3c
johnny gill <uint777@gmail.com>
2023-08-21 11:02:25 +0900

    부트스트랩 Select

--- coronaboard-web/src/pages/bootstrap/select.js
+++ coronaboard-web/src/pages/bootstrap/select.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Select from 'react-select/dist/declarations/src/Select';
+import { Container } from 'react-bootstrap';
+import Select from 'react-select';
 
-const option = [
+const options = [
   { value: 'KR', label: '한국' },
   { value: 'JP', label: '일본' },
   { value: 'US', label: '미국' },
@@ -12,6 +13,33 @@ const option = [
 const SelectPage = () => {
   const [selectedOptionSingle, setSelectedOptionSingle] = useState();
   const [selectedOptionMulti, setSelectedOptionMulti] = useState();
+
+  return (
+    <Container className="pt-3">
+      <h5>단일 선택 상자</h5>
+      <Select
+        value={selectedOptionSingle}
+        onChange={(selectedOption) => {
+          console.log('Single options selected', selectedOption);
+          setSelectedOptionSingle(selectedOption);
+        }}
+        options={options}
+      />
+      <hr />
+      <h5>다중 선택 상자</h5>
+      <Select
+        isMulti={true}
+        isSearchable={true}
+        placeholder="국가 선택..."
+        value={selectedOptionMulti}
+        onChange={(selectedOption) => {
+          console.log('Multiple options selected', selectedOption);
+          setSelectedOptionMulti(selectedOption);
+        }}
+        options={options}
+      />
+    </Container>
+  );
 };
 
 export default SelectPage;
