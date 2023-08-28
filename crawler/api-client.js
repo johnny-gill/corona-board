dab1eb2beb2a944a2f39024728dc0e8acde8791f
johnny gill <uint777@gmail.com>
2023-08-28 10:55:38 +0900

    api client 구현

---
+++ crawler/api-client.js
@@ -0,0 +1,26 @@
+const { default: axios } = require('axios');
+
+class ApiClient {
+  constructor() {
+    const client = axios.create({
+      baseURL: process.env.CB_API_BASE_URL || 'http://localhost:8080',
+    });
+
+    client.interceptors.response.use((resp) => resp.data);
+
+    this.client = client;
+  }
+
+  async upsertGlobalStat(data) {
+    return await this.client.post('global-stats', data);
+  }
+
+  async upsertKeyValue(key, value) {
+    return await this.client.post('key-value', {
+      key,
+      value,
+    });
+  }
+}
+
+module.exports = ApiClient;
